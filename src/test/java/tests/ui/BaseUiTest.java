152e4ef92f0f6adc093950d0eca2c1bf7133f385
Solosnyuk <asolosnyuk@mail.ru>
2025-08-18 17:52:18 +0300

    Update BaseUiTest.java

--- src/test/java/tests/ui/BaseUiTest.java
+++ src/test/java/tests/ui/BaseUiTest.java
@@ -21,7 +21,7 @@ public class BaseUiTest {
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() {
-        Configuration.browserSize = "1920x1080";
+        Configuration.headless = true;
         Selenide.open(RpConfig.getConfig().getEndpoint());
     }
 
